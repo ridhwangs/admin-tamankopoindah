bb991210f67c86908bd52d3e0d3188684656a87a
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2022-01-22 04:59:43 +0700

    http to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $project_title = '| By Smartcode';
         View::share('title', $project_title);
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
