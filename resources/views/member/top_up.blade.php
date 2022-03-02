2454b3087b479ab68a985a20bc35fac010e32d99
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2022-03-02 21:11:10 +0700

    merge

--- resources/views/member/top_up.blade.php
+++ resources/views/member/top_up.blade.php
@@ -43,7 +43,7 @@
                         @php
                             $status = [
                                 'approve' => 'success',
-                                'reject' => 'danger',
+                                'rejected' => 'danger',
                             ];
                         @endphp
                         <tr>
